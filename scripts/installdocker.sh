492d0a5e8f3399db4e5bd33e699014db6c37517b
cterrell133 <cterrell@kingdomtech.net>
2023-02-09 13:10:51 -0500

    intial commit with install script

---
+++ scripts/installdocker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+#Required packages 
+sudo yum install -y device-mapper-persistent-data lvm2
+#Install Docker
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+# Installing Most recent Version
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo docker version
