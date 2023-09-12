08dc991eba023e81cf033c338d17581050364752
cterrell133 <cterrell@kingdomtech.net>
2023-09-12 18:42:26 -0400

    working terraform templates

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 resource "aws_eks_node_group" "this" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.cluster_name}-node_group"
-  node_role_arn   = aws_iam_role.this.arn
+  node_role_arn   = aws_iam_role.eks-role.arn
   subnet_ids      = [var.subnet1, var.subnet2]
 
   scaling_config {
